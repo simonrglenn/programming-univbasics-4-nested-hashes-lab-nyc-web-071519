b05f8cf1e6ca6675f424d9ee601a36fe561ef08f
Simon Glenn <simonrglenn@gmail.com>
2019-07-01 23:18:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -132,7 +132,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:known_for]= ["Unix", "Assembly"]
+		 programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 
 		 programmer_hash
 end
